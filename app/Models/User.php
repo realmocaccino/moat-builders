0eafb9898ebb3c176b9c47e7b55886a9a00c8de4
LuizGustavo <lgustavoms@gmail.com>
2022-01-07 11:24:40 -0300

    User model and migration

--- app/Models/User.php
+++ app/Models/User.php
@@ -19,8 +19,9 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'name',
-        'email',
+        'username',
         'password',
+        'role'
     ];
 
     /**
@@ -29,16 +30,6 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $hidden = [
-        'password',
-        'remember_token',
-    ];
-
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
+        'password'
     ];
 }
