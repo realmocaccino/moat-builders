4ea81e7a46483773b8145324e5afb235c6330819
LuizGustavo <lgustavoms@gmail.com>
2022-01-09 15:36:17 -0300

    Add Artists link

--- resources/lang/en/layout.php
+++ resources/lang/en/layout.php
@@ -3,6 +3,7 @@
 return [
 
     'hello' => 'Hello',
+    'artists' => 'Artists',
     'albums' => 'Albums',
     'create_album' => 'Create Album',
     'logout' => 'Logout',
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -7,7 +7,7 @@
 	    <h1><a href="{{ route('home') }}">Moat Builders App</a></h1>
 	    @auth
 	        <p>@lang('layout.hello') {{ auth()->user()->name }}</p>
-	        <p><a href="{{ route('albums.index') }}">@lang('layout.albums')</a> | <a href="{{ route('albums.create') }}">@lang('layout.create_album')</a> | <a href="{{ route('logout') }}">@lang('layout.logout')</a></p>
+	        <p><a href="{{ route('home') }}">@lang('layout.artists')</a> | <a href="{{ route('albums.index') }}">@lang('layout.albums')</a> | <a href="{{ route('albums.create') }}">@lang('layout.create_album')</a> | <a href="{{ route('logout') }}">@lang('layout.logout')</a></p>
 	    @endauth
 		@yield('content')
 	</body>
