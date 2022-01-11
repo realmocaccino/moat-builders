5191dd8c69c4e9e2b6e89764d5cd4d305f5c26a7
LuizGustavo <lgustavoms@gmail.com>
2022-01-11 05:21:44 -0300

    Use app name from config

--- resources/views/layouts/private.blade.php
+++ resources/views/layouts/private.blade.php
@@ -1,7 +1,7 @@
 <!doctype html>
 <html>
 	<head>
-		<title>@yield('title') - Moat Builders Web Player</title>
+		<title>@yield('title') - {{ config('app.name') }}</title>
 		<link href="https://cdn.jsdelivr.net/npm/bootstrap@4.0/dist/css/bootstrap.min.css" rel="stylesheet">
 		<link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css" rel="stylesheet">
 		<link href="{{ asset('css/reset.css') }}" rel="stylesheet">
@@ -12,7 +12,7 @@
             <div class="row" id="header">
                 <div class="col-12">
                     <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                        <h1><a class="navbar-brand" href="{{ route('home') }}">Moat Builders Web Player</a></h1>
+                        <h1><a class="navbar-brand" href="{{ route('home') }}">{{ config('app.name') }}</a></h1>
                         <div class="collapse navbar-collapse" id="navbarSupportedContent">
                             <ul class="navbar-nav col-7">
                                 <li class="nav-item">
--- resources/views/layouts/public.blade.php
+++ resources/views/layouts/public.blade.php
@@ -1,7 +1,7 @@
 <!doctype html>
 <html>
 	<head>
-		<title>@yield('title') - Moat Builders Web Player</title>
+		<title>@yield('title') - {{ config('app.name') }}</title>
 		<link href="https://cdn.jsdelivr.net/npm/bootstrap@4.0/dist/css/bootstrap.min.css" rel="stylesheet">
 		<link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css" rel="stylesheet">
 		<link href="{{ asset('css/reset.css') }}" rel="stylesheet">
@@ -12,7 +12,7 @@
             <div class="row" id="header">
                 <div class="col-12">
                     <nav class="navbar navbar-expand-lg navbar-light bg-light justify-content-center">
-                        <h1><a class="navbar-brand" href="{{ route('login.index') }}">Moat Builders Web Player</a></h1>
+                        <h1><a class="navbar-brand" href="{{ route('login.index') }}">{{ config('app.name') }}</a></h1>
                     </nav>
                 </div>
             </div>
